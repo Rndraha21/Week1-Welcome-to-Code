23e1c933c8a97a75f5a37da38235dafc4b386487
Rndraha21 <robinndraha111@gmail.com>
2025-03-29 13:49:59 +0700

    Update

--- quiz/part3/tugas2.js
+++ quiz/part3/tugas2.js
@@ -1,4 +1,4 @@
-/* Soal 2
+/* Soal 2 Looping Menggunakan For
 Problem
 Pada tugas ini kamu diminta untuk melakukan looping dalam JavaScript dengan menggunakan syntax for. 
 Untuk membuat tantangan ini lebih menarik, kamu juga diminta untuk membuat suatu looping yang menghitung maju dan menghitung mundur. Jangan lupa tampilkan di console juga judul 'LOOPING PERTAMA' dan 'LOOPING KEDUA'.
